aaaf0f9dd688ff4ec551f038e496427e634ce5cb
abraham <abrahamgitonga98@gmail.com>
2023-11-27 22:21:34 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello chatty</h1>
+    </div>
+  );
+}
+
+export default App;
